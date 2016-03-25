abd5b44a2d288c0b7f0c0cca8a95e1aa002c1949
John Otander <johnotander@gmail.com>
2016-03-24 19:32:58 -0600

    Aggregate properties used

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ var select = require('postcss-select')
 var getClasses = require('get-classes-from-html')
 var removeEmpty = require('postcss-discard-empty')
 
+var props = {}
 module.exports = function tachyonify (html, css) {
   var classes = getClasses(html).map(function (klass) {
     return '.' + klass
   })
 
-  var usedCss = postcss([ select(classes), removeEmpty() ]).process(css).css
-  console.log(usedCss)
+  var usedCss = postcss([ select(classes), removeEmpty(), getProperties() ]).process(css).css
+  // console.log(usedCss)
+  console.log(props)
   return true
 }
+
+var getProperties = postcss.plugin('get-properties', function () {
+  return function (css, result) {
+    css.walkDecls(function (decl) {
+      props[decl.prop] = [] || props[decl.prop]
+      props[decl.prop].push(decl.value)
+    })
+  }
+})

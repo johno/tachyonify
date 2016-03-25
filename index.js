ab95995b46be0ec7676ab3ed688eb992f2d83098
John Otander <johnotander@gmail.com>
2016-03-24 19:18:39 -0600

    Find relevant CSS classes from html and css

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 'use strict'
 
-module.exports = function tachyonify (options) {
-  options = options || {}
+var fs = require('fs')
+var postcss = require('postcss')
+var select = require('postcss-select')
+var getClasses = require('get-classes-from-html')
+var removeEmpty = require('postcss-discard-empty')
 
+module.exports = function tachyonify (html, css) {
+  var classes = getClasses(html).map(function (klass) {
+    return '.' + klass
+  })
+
+  var usedCss = postcss([ select(classes), removeEmpty() ]).process(css).css
+  console.log(usedCss)
   return true
 }
--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 import test from 'ava'
+import fs from 'fs'
 import tachyonify from './'
 
 test('tachyonify does something awesome', t => {
   t.plan(1)
 
-  t.true(tachyonify())
+  const bootstrapCss = fs.readFileSync('node_modules/bootstrap/dist/css/bootstrap.css', 'utf8')
+  const output = tachyonify('<a href="#!" class="btn btn-default">Hello</a>', bootstrapCss)
+  t.true(false)
 })
